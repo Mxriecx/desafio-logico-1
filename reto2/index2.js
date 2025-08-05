8a9000fb67242ee5bddae66340ee89fffe3ee56b
Mxriecx <paztuiranmaria@gmail.com>
2025-08-04 21:54:16 -0500

    se crea funcion opciones para romper el codigo

--- reto2/index2.js
+++ reto2/index2.js
@@ -13,6 +13,7 @@ while (intentos < MAXIMODEINTENTOS) {
 
     if (pinIngresado === PIN) {
         alert("Pin correcto. Bienvenido a su cuenta");
+        opciones();
         break;
 
     } else {
@@ -24,48 +25,52 @@ while (intentos < MAXIMODEINTENTOS) {
 
     if (intentos === MAXIMODEINTENTOS) {
         alert("Supero el numero maximo de intentos permitidos. Acceso bloqueado,comuniquese con su banco");
-
+        
 
     }
 
 
 }
 
-// En este bloque de codigo con el uso del switch, el usuariop puede moverse dentro de las opciones del sistema, el bucle termina cuando la variable cajero pasa a ser falsa, en el aso 3 donde la opcion es salir//
-
-const opciones = prompt("Seleccione una opcion: \n1. ver saldo \n2.Retirar dinero \n3.Salir");
-
-let cajero = true;
-
-while (cajero) {
-
+function opciones(){
+    
+    // En este bloque de codigo con el uso del switch, el usuariop puede moverse dentro de las opciones del sistema, el bucle termina cuando la variable cajero pasa a ser falsa, en el aso 3 donde la opcion es salir//
+    
     const opciones = prompt("Seleccione una opcion: \n1. ver saldo \n2.Retirar dinero \n3.Salir");
-
-    switch (opciones) {
-
-        case "1":
-            alert(`Su saldo actual es : ${saldo}`);
-            break;
-
-        case "2":
-            const monto = prompt("Ingrese el monto a retirar");
-            if (monto <= 0) {
-                alert("Monto invalido, Intente de nuevo.");
-            } else if (monto > saldo) {
-                alert("Fondos insuficientes, Intente de nuevo");
-            } else {
-                saldo -= monto;
-                alert(`retiro exitoso. Nuevo saldo : ${saldo}`);
-            }
-            break;
-
-        case "3":
-            alert("Gracias por usar el sistema, vuelva pronto")
-            cajero = false;
-            break;
-        default:
-            alert("❌ Opción no válida. Intente de nuevo.");
+    
+    let cajero = true;
+    
+    while (cajero) {
+    
+        const opciones = prompt("Seleccione una opcion: \n1. ver saldo \n2.Retirar dinero \n3.Salir");
+    
+        switch (opciones) {
+    
+            case "1":
+                alert(`Su saldo actual es : ${saldo}`);
+                break;
+    
+            case "2":
+                const monto = prompt("Ingrese el monto a retirar");
+                if (monto <= 0) {
+                    alert("Monto invalido, Intente de nuevo.");
+                } else if (monto > saldo) {
+                    alert("Fondos insuficientes, Intente de nuevo");
+                } else {
+                    saldo -= monto;
+                    alert(`retiro exitoso. Nuevo saldo : ${saldo}`);
+                }
+                break;
+    
+            case "3":
+                alert("Gracias por usar el sistema, vuelva pronto")
+                cajero = false;
+                break;
+            default:
+                alert("❌ Opción no válida. Intente de nuevo.");
+        }
     }
+    
+    
+    
 }
-
-
